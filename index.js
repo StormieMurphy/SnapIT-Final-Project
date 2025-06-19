f561417711102b3b9a793fd68be6d7d87fd63a54
StormieMurphy <stolenmurphy@gmail.com>
2025-06-19 13:15:54 -0500

    Created Ice Cream Shop Logo

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 // Import external packages, modules, and functions. prompt-sync is an external package. You need to execute `npm install prompt-sync@4.2.0` in the terminal to install it.
-
+// const prompt = require("prompt-sync")({ sigint: true });
+// const {
+//   checkYorN,
+//   chooseYourCone,
+//   chooseYourFlavorType,
+//   chooseYourFruit,
+//   chooseYourSavory,
+//   chooseYourChocolate,
+//   getTotalCost
+// } = require("./helpers.js");
+// const iceCream = require("./IceCream.js");
 // Define necessary variables for the program.
+const shopTitle =
+  "==================================" +
+  "\n||\t\t\t\t||\n||\t    LISA\'s ICE CREAM\t||\n||\t\t\t\t||\n" +
+  "==================================";
+const welcomeStr = "Welcome to Lisa's Ice Cream shop!\n";
 
 // Print the shop title and welcome message to the console.
 
+console.log(shopTitle);
+console.log(welcomeStr);
+
 // Prompt a user for an input.
 
-// If the user's input is invalid, keep asking until a valid input is provided.
+// let userInput = prompt("Would you like to make an order Y or N? ");
+
+// // If the user's input is invalid, keep asking until a valid input is provided.
+
+// while (!checkYorN(userInput)) {
+//   userInput = prompt('Please enter letter "Y" or letter "N": ');
+// }
 
 // Check the user's input to whether or not run the program.
+
 
 // Get all the necessary data from the user to create an ice cream cone.
 
